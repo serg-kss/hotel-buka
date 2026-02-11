45eb657e38804d9de5a8de43182b51d534a7236d
GetApple <getapple@MacBook-Air-KSS.local>
2026-02-11 12:16:17 +0200

    locale

--- hotel/settings.py
+++ hotel/settings.py
@@ -8,7 +8,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
