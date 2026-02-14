8b3a5c1aa44ba619a82c84480ad36c2eb900f599
GetApple <getapple@MacBook-Air-KSS.local>
2026-02-14 19:39:00 +0200

    creation admin render1

--- hotel/settings.py
+++ hotel/settings.py
@@ -152,29 +152,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-import os
-from django.contrib.auth import get_user_model
-from django.db.utils import OperationalError
-
-def create_superuser():
-    if os.getenv("CREATE_SUPERUSER") == "True":
-        try:
-            User = get_user_model()
-            username = os.getenv("DJANGO_SUPERUSER_USERNAME")
-            email = os.getenv("DJANGO_SUPERUSER_EMAIL")
-            password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-
-            if username and password:
-                if not User.objects.filter(username=username).exists():
-                    User.objects.create_superuser(
-                        username=username,
-                        email=email,
-                        password=password
-                    )
-                    print("Superuser created successfully")
-        except OperationalError:
-            # база ещё не готова
-            pass
-
-create_superuser()
 
