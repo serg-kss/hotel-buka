56cab056f921193f8ea37798d75932f3ba2bb18b
GetApple <getapple@MacBook-Air-KSS.local>
2026-02-25 12:01:37 +0200

    url correction

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,14 +1,12 @@
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import include, path
-from django.shortcuts import redirect
-from django.views.i18n import set_language
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.utils.translation import get_language_from_request
+from django.conf.urls.i18n import i18n_patterns
 from django.views.generic import TemplateView
-from django.contrib.sitemaps.views import sitemap
-from main.sitemaps import StaticViewSitemap, RoomSitemap
+from django.utils.translation import get_language_from_request
+from django.shortcuts import redirect
+from django.conf import settings
 
 
 def root_redirect(request):
@@ -18,39 +16,28 @@ def root_redirect(request):
     return redirect(f"/{lang}/", permanent=True)
 
 
-sitemaps = {
-    "static": StaticViewSitemap,
-    "rooms": RoomSitemap,
-}
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("i18n/setlang/", set_language, name="set_language"),
-    path("", root_redirect),
-    path(
-        "robots.txt",
-        TemplateView.as_view(
-            template_name="robots.txt",
-            content_type="text/plain",
-        ),
-    ),
+    path("robots.txt", TemplateView.as_view(
+        template_name="robots.txt",
+        content_type="text/plain"
+    )),
 ]
-
 
 urlpatterns += i18n_patterns(
     path("", include(("main.urls", "main"), namespace="main")),
     path("", include(("booking.urls", "booking"), namespace="booking")),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps, "template_name": "sitemap.xml",}, name="django.contrib.sitemaps.views.sitemap"),
-
     prefix_default_language=True,
 )
+
+
+urlpatterns += [
+    path("", root_redirect),
+]
+
 
 urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
-
-
-#if settings.DEBUG:
-    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
